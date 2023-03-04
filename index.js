0e5f7ea302e3ce510c3e5f0db25834a76cd3d7a5
Sushil Gupta <kalwarsushil2@gmail.com>
2023-03-04 11:29:27 +0545

    first

---
+++ index.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+require('express-async-errors')
+
+//
+const app = express()
+
+// enable to .env
+dotenv.config()
+
+// DB File
+const connectDB = require('./db/dbConnect')
+
+// routes
+const authRoute = require('./routes/authRoute')
+const productRoute = require('./routes/productRoute')
+
+app.use(express.json())
+app.use(cors())
+
+//
+app.use('/api/auth', authRoute)
+app.use('/api/products', productRoute)
+
+// SERVER
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server running on : ' + process.env.PORT)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
